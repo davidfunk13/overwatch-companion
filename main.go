3a2d342cc931fa355f4de361dc538701b0e47790
Dave <davefunk135@gmail.com>
2021-01-24 22:29:32 -0800

    testing what might be wrong with deplyoment

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	app := fiber.New()
 	app.Get("/", func(c *fiber.Ctx) error {
+		fmt.Print("Testing")
 		return c.SendString("Hello, World!")
 	})
 	app.Listen(":3000")
