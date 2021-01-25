290311e5489a2c4c358d6e29ba7c94c589829147
Dave <davefunk135@gmail.com>
2021-01-24 23:18:33 -0800

    trying to figure out go env variables.

--- main.go
+++ main.go
@@ -2,15 +2,21 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"reflect"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	app := fiber.New()
+	port := os.Getenv("PORT")
+	env := os.Getenv("APP_ENV")
+	fmt.Println(reflect.TypeOf(port))
+	fmt.Println(reflect.TypeOf(env))
 	app.Get("/", func(c *fiber.Ctx) error {
 		fmt.Print("Testing")
 		return c.SendString("Hello, World!")
 	})
-	app.Listen(":3000")
+	app.Listen(port)
 }
