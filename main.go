877e60d5022e1b9381fef466c69b75cb604d0283
Dave <davefunk135@gmail.com>
2021-01-26 21:12:36 -0800

    testing cors configuration

--- main.go
+++ main.go
@@ -6,10 +6,16 @@ import (
 
 	"github.com/davidfunk13/overwatch-companion/api"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://overwatch-companion.herokuapp.com, http://localhost:3000, http://localhost:3001",
+	}))
+
 	port := os.Getenv("PORT")
 
 	//if is dev
