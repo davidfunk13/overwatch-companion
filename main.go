4dbebc28a81a81858a91a5d2a97d36f0d43e2500
Dave <davefunk135@gmail.com>
2021-02-03 23:46:43 -0800

    testing

--- main.go
+++ main.go
@@ -15,22 +15,6 @@ import (
 
 const defaultPort = "3001"
 
-// CORS Middleware
-func CORS(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		origin := r.Header.Get("Origin")
-		w.Header().Set("Access-Control-Allow-Origin", origin)
-		if r.Method == "OPTIONS" {
-			w.Header().Set("Access-Control-Allow-Credentials", "true")
-			w.Header().Set("Access-Control-Allow-Methods", "GET,POST")
-
-			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, X-CSRF-Token, Authorization")
-			return
-		}
-
-		h.ServeHTTP(w, r)
-	})
-}
 func main() {
 	port := os.Getenv("PORT")
 
@@ -49,7 +33,7 @@ func main() {
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", CORS(srv))
+	http.Handle("/query", srv)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
