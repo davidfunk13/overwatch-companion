3b6158d35922a78809eef2419c89eb6dce4cf50a
Dave <davefunk135@gmail.com>
2021-02-23 19:55:01 -0800

    trying out a new way to handle cors

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 	"github.com/davidfunk13/overwatch-companion/database"
 	router "github.com/davidfunk13/overwatch-companion/router"
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/gorilla/handlers"
 	"github.com/joho/godotenv"
 )
 
@@ -16,9 +15,6 @@ const defaultPort = "3001"
 
 func main() {
 	env := os.Getenv("APP_ENV")
-	headersOk := handlers.AllowedHeaders([]string{"X-Requested-With"})
-	originsOk := handlers.AllowedOrigins([]string{os.Getenv("ALLOWED_ORIGIN")})
-	methodsOk := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS"})
 
 	if env != "production" {
 		err := godotenv.Load()
@@ -28,6 +24,7 @@ func main() {
 	}
 
 	port := os.Getenv("PORT")
+
 	if port == "" {
 		port = defaultPort
 	}
@@ -36,11 +33,11 @@ func main() {
 
 	r := router.NewRouter()
 
-	http.Handle("/", r)
+	http.Handle("/", &router.CORSRouterDecorator{R: r})
 
 	if env != "production" {
 		log.Printf("welcome to dev mode. connect to http://localhost:%s/dev for GraphQL playground", port)
 	}
 
-	log.Fatal(http.ListenAndServe(":"+port, handlers.CORS(headersOk, originsOk, methodsOk)(r)))
+	log.Fatal(http.ListenAndServe(":"+port, r))
 }
--- router/router.go
+++ router/router.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
@@ -13,15 +14,34 @@ import (
 	"github.com/urfave/negroni"
 )
 
-// NewRouter establishes all handlers for api routes
-func NewRouter() *mux.Router {
-	r := mux.NewRouter()
-	addHandlers(r)
-	return r
+// CORSRouterDecorator applies CORS headers to a mux.Router
+type CORSRouterDecorator struct {
+	R *mux.Router
 }
 
-func addHandlers(r *mux.Router) {
+// ServeHTTP wraps the HTTP server enabling CORS headers.
+// For more info about CORS, visit https://www.w3.org/TR/cors/
+func (c *CORSRouterDecorator) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	if origin := req.Header.Get("Origin"); origin != "" {
+		rw.Header().Set("Access-Control-Allow-Origin", origin)
+		rw.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		rw.Header().Set("Access-Control-Allow-Headers", "Accept, Accept-Language, Content-Type, YourOwnHeader")
+	}
+	// Stop here if its Preflighted OPTIONS request
+	if req.Method == "OPTIONS" {
+		return
+	}
+
+	c.R.ServeHTTP(rw, req)
+}
+
+// NewRouter establishes all handlers for api routes
+func NewRouter() *mux.Router {
+	//get the current environment
 	env := os.Getenv("APP_ENV")
+
+	//create new router
+	r := mux.NewRouter()
 
 	//graphql server
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
@@ -30,16 +50,17 @@ func addHandlers(r *mux.Router) {
 	if env != "production" {
 		fmt.Println("You are in development env.", env)
 		//set up the playground for graph queries
-		r.Handle("/dev", playground.Handler("GraphQL playground", os.Getenv("GRAPH_API")))
+		r.Handle("/dev", playground.Handler("GraphQL playground", os.Getenv("GRAPH_SERVER")))
 	}
 
 	//if we're in production, instead put the graphql server in a Negroni instance with our jwt middleware.
 	queryHandler := negroni.New(negroni.HandlerFunc(auth.JWTMiddleware.HandlerWithNext), negroni.Wrap(srv))
 
 	//serve graphql server at api endpoint.
-	r.Handle(os.Getenv("GRAPH_API"), queryHandler)
+	r.Handle(os.Getenv("GRAPH_SERVER"), queryHandler)
 
 	//we should do a profile endpoint with the auth0 management api here.
 
-	//think about how we could use the scope funcit
+	//think about how we could use the scope funcits
+	return r
 }
